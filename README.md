4ce575fdd6fe4937a0121f6b99919a5ccba01a2c
Harm Roukema <harmroukema@gmail.com>
2026-01-26 12:15:28 +0100

    Argument parsing, todo list

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # oidc-learning-based-fuzzing
+
+## TODO
+- [x] Add command line arguments
+- [ ] Expand to OIDC flow
+- [ ] Automatically parse discovery endpoint
+- [ ] Improve fuzzing capabilities
+    - [ ] URL fuzzer (redirect_uri, request_uri)
+    - [ ] Basic parameter fuzzer:
+        - [ ] Change casing
+        - [ ] Change 1 character
+        - [ ] Append a character
+        - [ ] Append a special character
+        - [ ] Parameter pollution: duplicate, two different parameters, maybe with arrays? redirect_uri[0]=x,redirect_uri[1]=y?
+    - [ ] JWT fuzzer (use jwt tool as inspiration)
+    - [ ] OAuthTester like approach: define properties of parameters and fuzz based on these properties:
+        - constant: Compare the values between different sessions and users
+        - mandatory parameter: Remove this parameter and randomize its values
+        - used for once or multiple times: Substitute the value with an used one and compare the response
+        - user-specific: Substitute the value with a fresh one of another user
+        - session-specific: Open a new browser and get a fresh value of this parameter to substitute the existing one
--- learn-oidc.py
+++ learn-oidc.py
@@ -2,12 +2,15 @@ import requests
 import time
 from requests.adapters import HTTPAdapter
 from urllib.parse import urlparse
+import re
+import argparse
+import datetime
 
 from aalpy.base import SUL
 from aalpy.oracles import RandomWalkEqOracle, StatePrefixEqOracle
 from aalpy.learning_algs import run_Lstar
 from aalpy.utils import visualize_automaton, save_automaton_to_file, load_automaton_from_file
-import re
+
 
 requests.packages.urllib3.disable_warnings()
 
@@ -59,25 +62,19 @@ def make_request_with_retry(session, method, url, max_retries=5, **kwargs):
             print(f"Max retries exceeded for {url}")
             raise e
 
-proxies = {
-    "http": "http://127.0.0.1:8080",
-    "https": "http://127.0.0.1:8080"
-}
-
 class OAuthSUL(SUL):
-    def __init__(self):
+    def __init__(self, auth_server_url, client_url, proxy=None):
         super().__init__()
-        self.CLIENT_URL="https://client1-oauth-ctf.harmroukema.nl"
-        self.CLIENT_ID="vulnclient"
-        self.AUTH_SERVER_URL="https://server1-oauth-ctf.harmroukema.nl"
-        self.DOMAIN="harmroukema.nl"
+        self.CLIENT_URL=client_url
+        self.AUTH_SERVER_URL=auth_server_url
+        if proxy:
+            self.proxies = {
+                "http": proxy,
+                "https": proxy
+            }
+        else:
+            self.proxies = None
         self.s = requests.Session()
-        # self.client_id = None
-        # self.code = None
-        # self.state = None
-        # self.redirect_uri = None
-        # self.response_type = "code"
-        # self.redirect = None
 
     def pre(self):
         self.s = requests.Session()
@@ -112,72 +109,72 @@ class OAuthSUL(SUL):
                     'response_type': 'code', # Default response type
                 }
                 url = f"{self.CLIENT_URL}/sso_login"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 self._parse_redirect_params(r)
                 
                 return abstract_output(r)
             
             case "client_callback":
                 url = f"{self.CLIENT_URL}/callback?code={self.parsed_params.get('code')}&state={self.parsed_params.get('state')}"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 return abstract_output(r)
             
             case "client_callback_invalid_state":
                 url = f"{self.CLIENT_URL}/callback?code={self.parsed_params.get('code')}&state=invalidstate"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 return abstract_output(r)
             
             case "client_callback_error":
                 url = f"{self.CLIENT_URL}/callback?error=error&state={self.parsed_params.get('state')}"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 return abstract_output(r)
             
             case "authserver_authorize":
                 url = f"{self.AUTH_SERVER_URL}/authorize?client_id={self.parsed_params.get('client_id')}&redirect_uri={self.parsed_params.get('redirect_uri')}&response_type={self.parsed_params.get('response_type')}&state={self.parsed_params.get('state')}"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 self._parse_redirect_params(r)
 
                 return abstract_output(r)
             
             case "authserver_authorize_invalid_client":
                 url = f"{self.AUTH_SERVER_URL}/authorize?client_id=invalidclient&redirect_uri={self.parsed_params.get('redirect_uri')}&response_type={self.parsed_params.get('response_type')}&state={self.parsed_params.get('state')}"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 self._parse_redirect_params(r)
 
                 return abstract_output(r)
             
             case "authserver_authorize_invalid_redirect_uri":
                 url = f"{self.AUTH_SERVER_URL}/authorize?client_id={self.parsed_params.get('client_id')}&response_type={self.parsed_params.get('response_type')}&state={self.parsed_params.get('state')}"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 self._parse_redirect_params(r)
 
                 return abstract_output(r)
             
             case "authserver_authorize_unsupported_response_type":
                 url = f"{self.AUTH_SERVER_URL}/authorize?client_id={self.parsed_params.get('client_id')}&redirect_uri={self.parsed_params.get('redirect_uri')}&response_type=unsupported&state={self.parsed_params.get('state')}"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 self._parse_redirect_params(r)
 
                 return abstract_output(r)
             
             case "authserver_login":
                 url = f"{self.AUTH_SERVER_URL}/login?redirect=/"
-                r = make_request_with_retry(self.s, 'POST', url, data={'username': 'user', 'password': 'password'}, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'POST', url, data={'username': 'user', 'password': 'password'}, proxies=self.proxies, verify=False)
                 self._parse_redirect_params(r)
                 
                 return abstract_output(r)
             
             case "authserver_login_invalid_credentials":
                 url = f"{self.AUTH_SERVER_URL}/login?redirect=/"
-                r = make_request_with_retry(self.s, 'POST', url, data={'username': 'user', 'password': 'wrongpassword'}, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'POST', url, data={'username': 'user', 'password': 'wrongpassword'}, proxies=self.proxies, verify=False)
                 self._parse_redirect_params(r)
 
                 return abstract_output(r)
 
 
 class FuzzingSUL(OAuthSUL):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, auth_server_url, client_url, proxy=None):
+        super().__init__(auth_server_url=auth_server_url, client_url=client_url, proxy=proxy)
         self.concrete_trace = []  # Stores concrete fuzzed values for reproducibility
 
     def pre(self):
@@ -204,7 +201,7 @@ class FuzzingSUL(OAuthSUL):
                 concrete_value = {'fuzzed_redirect_uri': fuzzed_uri}
                 
                 url = f"{self.AUTH_SERVER_URL}/authorize?client_id={self.parsed_params.get('client_id')}&redirect_uri={fuzzed_uri}&response_type={self.parsed_params.get('response_type')}&state={self.parsed_params.get('state')}"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 self._parse_redirect_params(r)
                 
                 self.concrete_trace.append(concrete_value)
@@ -225,7 +222,7 @@ class FuzzingSUL(OAuthSUL):
                     fuzzed_uri = self.fuzz_redirect_uri(self.parsed_params.get('redirect_uri'))
                 
                 url = f"{self.AUTH_SERVER_URL}/authorize?client_id={self.parsed_params.get('client_id')}&redirect_uri={fuzzed_uri}&response_type={self.parsed_params.get('response_type')}&state={self.parsed_params.get('state')}"
-                r = make_request_with_retry(self.s, 'GET', url, proxies=proxies, verify=False)
+                r = make_request_with_retry(self.s, 'GET', url, proxies=self.proxies, verify=False)
                 self._parse_redirect_params(r)
                 return abstract_output(r)
             
@@ -233,7 +230,7 @@ class FuzzingSUL(OAuthSUL):
                 # For non-fuzzed inputs, use parent implementation
                 return super().step(letter)
 
-sul = OAuthSUL()
+
 input_al = ["client_sso_login", 
             "client_callback", 
             "client_callback_invalid_state", 
@@ -246,33 +243,74 @@ input_al = ["client_sso_login",
             "authserver_login_invalid_credentials"
             ]
 
-# eq_oracle = RandomWalkEqOracle(input_al, sul, num_steps=1000, reset_after_cex=False, reset_prob=0.15)
 
-# print("Starting learning...")
-# learned_model = run_Lstar(input_al, sul, eq_oracle, automaton_type='mealy', cache_and_non_det_check=True, print_level=3)
+def learn_model(sul):
+    eq_oracle = RandomWalkEqOracle(input_al, sul, num_steps=1000, reset_after_cex=False, reset_prob=0.15)
 
-# save_automaton_to_file(learned_model, 'learned_oauth_model.dot')
-# visualize_automaton(learned_model, path='learned_oauth_model', file_type='pdf')
+    print("Starting learning...")
+    learned_model = run_Lstar(input_al, sul, eq_oracle, automaton_type='mealy', cache_and_non_det_check=True, print_level=3)
 
-learned_model = load_automaton_from_file('learned_oauth_model.dot', automaton_type='mealy', compute_prefixes=True)
-fuzzing_sul = FuzzingSUL()
-eo = StatePrefixEqOracle(input_al, fuzzing_sul, walks_per_state=20, walk_len=10)
-cex = eo.find_cex(learned_model)
-if cex:
-    print("Counterexample found")
-    print("Inputs values", cex)
-    print("Concrete values", fuzzing_sul.concrete_trace)
+    return learned_model
 
-    # Save concrete trace before resetting
-    saved_concrete_trace = fuzzing_sul.concrete_trace.copy()
 
-    learned_model.reset_to_initial()
-    output_base = [learned_model.step(i) for i in cex]
+def fuzz_model(fuzzing_sul, learned_model):
+    eo = StatePrefixEqOracle(input_al, fuzzing_sul, walks_per_state=20, walk_len=10)
+    cex = eo.find_cex(learned_model)
+    if cex:
+        print("Counterexample found")
+        print("Inputs values", cex)
+        print("Concrete values", fuzzing_sul.concrete_trace)
+
+        # Save concrete trace before resetting
+        saved_concrete_trace = fuzzing_sul.concrete_trace.copy()
+
+        learned_model.reset_to_initial()
+        output_base = [learned_model.step(i) for i in cex]
+        
+        fuzzing_sul.post()
+        fuzzing_sul.pre()
+
+        output_sul = [fuzzing_sul.trace_with_concrete_values(i, c) for i, c in zip(cex, saved_concrete_trace)]
+
+        print("Model Output", output_base)
+        print("SUL Output", output_sul)
+
+
+def parse_discovery_endpoint(discovery_url: str):
+    pass
+
+
+def setup_argparse():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    parser = argparse.ArgumentParser(description="Learn and fuzz an OIDC implementation with AALpy")
+
+    parser.add_argument('auth_server_url', type=str, help='Base URL of the Authorization Server (e.g., http://localhost:5000)')
+    parser.add_argument('client_url', type=str, help='Base URL of the Client Application (e.g., http://localhost:5001)')
+
+    parser.add_argument('-l', '--load-model', type=str, help='Path to load existing model from .dot file, skips learning if provided')
+    parser.add_argument('-s', '--save-model', type=str, help='Path to save learned model to .dot file', default=f'{timestamp}.dot')
+    parser.add_argument('-nv', '--no-visualize', action='store_true', help='Do not visualize the learned model as a PDF')
+    parser.add_argument('--only-learn', action='store_true', help='Only perform learning, skip fuzzing')
+    parser.add_argument('-p', '--proxy', type=str, help='Proxy URL to use for requests (e.g., http://127.0.0.1:8080)')
+
+    return parser
+
+
+if __name__ == "__main__":
+    parser = setup_argparse()
+    args = parser.parse_args()
+
+    if args.load_model:
+        learned_model = load_automaton_from_file(args.load_model, automaton_type='mealy', compute_prefixes=True)
+    else:
+        sul = OAuthSUL(auth_server_url=args.auth_server_url, client_url=args.client_url, proxy=args.proxy)
+        learned_model = learn_model(sul)
+        if args.save_model:
+            save_automaton_to_file(learned_model, args.save_model)
     
-    fuzzing_sul.post()
-    fuzzing_sul.pre()
+    if not args.no_visualize:
+        visualize_automaton(learned_model, path=args.save_model.rsplit('.', 1)[0], file_type='pdf')
 
-    output_sul = [fuzzing_sul.trace_with_concrete_values(i, c) for i, c in zip(cex, saved_concrete_trace)]
-
-    print("Model Output", output_base)
-    print("SUL Output", output_sul)   
+    if not args.only_learn:
+        fuzzing_sul = FuzzingSUL(auth_server_url=args.auth_server_url, client_url=args.client_url, proxy=args.proxy)
+        fuzz_model(fuzzing_sul, learned_model)
